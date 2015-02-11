acf63db255b4d11a28262ec731cbd5a010e684c1
Raül Pérez <repejota@gmail.com>
2015-02-11 18:33:16 +0100

    Fixed node version

--- cookbooks/op/recipes/nodejs.rb
+++ cookbooks/op/recipes/nodejs.rb
@@ -2,8 +2,8 @@ bash 'nodejs' do
     user "root"
     cwd "/tmp"
     code <<-EOH
-    rm -rf node-v0.12.00-linux-x64.tar.gz
-    wget http://nodejs.org/dist/v0.12.00/node-v0.12.00-linux-x64.tar.gz
-    cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.00-linux-x64.tar.gz
+    rm -rf node-v0.12.0-linux-x64.tar.gz
+    wget http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz
+    cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.0-linux-x64.tar.gz
     EOH
 end
