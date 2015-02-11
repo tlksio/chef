9f1703e7935b5ea114e322cbe312efa8c7282851
Raül Pérez <repejota@gmail.com>
2015-02-11 18:27:45 +0100

    Cleanup and moving to tlks

--- cookbooks/op/recipes/nodejs.rb
+++ cookbooks/op/recipes/nodejs.rb
@@ -3,7 +3,7 @@ bash 'nodejs' do
     cwd "/tmp"
     code <<-EOH
     rm -rf node-v0.12.00-linux-x64.tar.gz
-    wget http://nodejs.org/dist/v0.10.35/node-v0.12.00-linux-x64.tar.gz
+    wget http://nodejs.org/dist/v0.12.00/node-v0.12.00-linux-x64.tar.gz
     cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.00-linux-x64.tar.gz
     EOH
 end
