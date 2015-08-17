a0b18fe2bb0c2fe0eb86eb9aad017f4afed2bce5
Raül Pérez <repejota@gmail.com>
2015-08-18 01:49:50 +0200

    Updated path for golang

--- cookbooks/op/recipes/golang.rb
+++ cookbooks/op/recipes/golang.rb
@@ -4,6 +4,7 @@ bash 'nodejs' do
     code <<-EOH
     rm -rf go1.4.2.linux-amd64.tar.gz
     wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+    mkdir -p /usr/local/go
     cd /usr/local/go && tar --strip-components 1 -xzf /tmp/go1.4.2.linux-amd64.tar.gz
     EOH
 end
