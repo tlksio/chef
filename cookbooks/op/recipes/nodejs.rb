ddeee5d999d9ea263e31ce663c66252b36489d60
Raül Pérez <repejota@gmail.com>
2015-08-18 01:04:03 +0200

    Upgraded to node 0.12.7

--- cookbooks/op/recipes/nodejs.rb
+++ cookbooks/op/recipes/nodejs.rb
@@ -2,9 +2,9 @@ bash 'nodejs' do
     user "root"
     cwd "/tmp"
     code <<-EOH
-    rm -rf node-v0.12.0-linux-x64.tar.gz
-    wget http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz
-    cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.0-linux-x64.tar.gz
+    rm -rf node-v0.12.*-linux-x64.tar.gz
+    wget http://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz
+    cd /usr/local && tar --strip-components 1 -xzf /tmp/node-v0.12.7-linux-x64.tar.gz
     setcap cap_net_bind_service=+ep /usr/local/bin/node
     EOH
 end
